fac9640b1aedfb12b5c62a675b886d09155c850e
moboz <metinoktayboz@gmail.com>
2025-05-06 12:47:43 +0300

    Initial commit

---
+++ src/test/java/com/moboz/ss11/Ss11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moboz.ss11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
