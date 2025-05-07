c8ef1d3e29d0b176d2b99e12ef4f6c373575c76c
moboz <metinoktayboz@gmail.com>
2025-05-07 17:49:04 +0300

    Laur Splica's Spring Security Fundamentals Examples - L12 - The OAuth2 Authorization Server P2 (Customize token, providerValidator and opaque tokens)

--- src/main/java/com/moboz/ss11/config/SecurityConfig.java
+++ src/main/java/com/moboz/ss11/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,12 +18,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationProvider;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -31,7 +37,10 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @Configuration
 public class SecurityConfig {
@@ -43,6 +52,7 @@ public class SecurityConfig {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .authorizationEndpoint( a -> a.authenticationProviders(getAuthorizationEndpointProviders()))
                 .oidc(Customizer.withDefaults());
 
 
@@ -50,6 +60,17 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    private Consumer<List<AuthenticationProvider>> getAuthorizationEndpointProviders() {
+        return providers -> {
+            for (AuthenticationProvider provider : providers) {
+                if(provider instanceof OAuth2AuthorizationCodeRequestAuthenticationProvider p){
+                    p.setAuthenticationValidator(new CustomRedirectUriValidator());
+                }
+            }
+        };
+    }
+
 
     @Bean
     @Order(2)
@@ -86,6 +107,12 @@ public class SecurityConfig {
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .tokenSettings(
+//                        TokenSettings.builder()
+//                                .accessTokenFormat(OAuth2TokenFormat.REFERENCE) //for opaque token
+//                                .accessTokenTimeToLive(Duration.ofSeconds(900))
+//                                .build()
+//                )
                 .build();
 
         return new InMemoryRegisteredClientRepository(r1);
@@ -112,5 +139,13 @@ public class SecurityConfig {
 
         JWKSet set = new JWKSet(key);
         return new ImmutableJWKSet<>(set);
+    }
+
+
+    @Bean
+    OAuth2TokenCustomizer<JwtEncodingContext> oauth2TokenCustomizer() {
+        return context -> {
+            context.getClaims().claim("test", "test");
+        };
     }
 }
