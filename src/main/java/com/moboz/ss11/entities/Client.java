0aa54b22873d6dec339ef0e5e14ecd6c2cacf756
moboz <metinoktayboz@gmail.com>
2025-05-14 16:21:42 +0300

    Laur Splica's Spring Security Fundamentals Examples - L16 Updates (OAuth2 Client)

--- src/main/java/com/moboz/ss11/entities/Client.java
+++ src/main/java/com/moboz/ss11/entities/Client.java
@@ -107,6 +107,7 @@ public class Client {
                 .redirectUri(client.getRedirectUri())
                 .clientAuthenticationMethod(new ClientAuthenticationMethod(client.getAuthMethod()))
                 .authorizationGrantType(new AuthorizationGrantType(client.getGrantType()))
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                 .tokenSettings(TokenSettings.builder()
                         .accessTokenFormat(OAuth2TokenFormat.REFERENCE) //opaque
                         .accessTokenTimeToLive(Duration.ofHours(24))
